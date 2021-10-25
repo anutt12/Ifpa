2c38f787d153f8f584f21d8992d5133d3d409f5d
Andrew Nutt <anutt@paypal.com>
2021-10-25 11:13:31 -0700

    added resources

--- src/main/java/com/pinball/ifpa/crawler/Scraper.java
+++ src/main/java/com/pinball/ifpa/crawler/Scraper.java
@@ -1,4 +1,9 @@
 package com.pinball.ifpa.crawler;
 
+import org.jsoup.Jsoup;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 public class Scraper {
+
+    Document page = Jsoup.connect("http://books.toscrape.com/index.html").get();
 }
