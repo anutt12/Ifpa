85ea19e27ed01dba31142682591b2c77c4a36609
Andrew Nutt <anutt@paypal.com>
2021-10-25 11:03:17 -0700

    another attempt

---
+++ src/main/java/com/pinball/ifpa/IfpaApplication.java
@@ -0,0 +1,13 @@
+package com.pinball.ifpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pinball/ifpa/IfpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinball.ifpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
