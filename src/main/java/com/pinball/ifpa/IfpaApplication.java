0fd36581aa0640caf0d5eae753f60e57651bb3e9
Andrew Nutt <anutt@paypal.com>
2021-10-25 11:25:59 -0700

    initial scrape test successful

--- src/main/java/com/pinball/ifpa/IfpaApplication.java
+++ src/main/java/com/pinball/ifpa/IfpaApplication.java
@@ -1,13 +1,40 @@
 package com.pinball.ifpa;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class IfpaApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(IfpaApplication.class, args);
+
+        Document page = Jsoup.connect("http://books.toscrape.com/index.html").get();
+
+        //selecting all hyperlinks
+        Elements pageElements = page.select("a[href]");
+
+        ArrayList<String> hyperLinks = new ArrayList<String>();
+
+        //iterating and extracting
+        for (Element e:pageElements) {
+            hyperLinks.add("Text: " + e.text());
+            hyperLinks.add("Link: " + e.attr("href"));
+        }
+
+        for (String s : hyperLinks) {
+            System.out.println(s);
+        }
+
     }
+
+
 
 }
