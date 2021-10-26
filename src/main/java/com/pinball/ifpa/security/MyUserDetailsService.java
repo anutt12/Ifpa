72160bda674bdd87d45791e2d9b87db71784ebe1
Andrew Nutt <anutt@paypal.com>
2021-10-26 16:22:53 -0700

    added security package

---
+++ src/main/java/com/pinball/ifpa/security/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.pinball.ifpa.security;
+
+import com.pinball.ifpa.model.User;
+import com.pinball.ifpa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmail(email);
+        return new MyUserDetails(user);
+    }
+}
