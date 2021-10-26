a622e8756d86855b1e2167f3eb2cd998e859a82d
Matty Thompson <matthompson@paypal.com>
2021-10-26 12:45:46 -0500

    "Pulling master"

--- src/main/java/com/pinball/ifpa/IfpaApplication.java
+++ src/main/java/com/pinball/ifpa/IfpaApplication.java
@@ -5,6 +5,7 @@ import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.DomNode;
 import com.gargoylesoftware.htmlunit.html.DomNodeList;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,10 +13,13 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.gargoylesoftware.htmlunit.*;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class IfpaApplication {
@@ -23,24 +27,23 @@ public class IfpaApplication {
     public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
         SpringApplication.run(IfpaApplication.class, args);
 
-//        Document page = Jsoup.connect("http://books.toscrape.com/index.html").get();
+//        Document page = Jsoup.connect("https://www.barnesandnoble.com/b/books/fiction/thrillers/_/N-1z0zta0Z29Z8q8Z1d3u?Nrpp=20&page=1").get();
 //
 //        //selecting all hyperlinks
-//        Elements pageElements = page.select("a[href]");
+//        Elements pageElements = page.select("a[title]");
 //
 //        ArrayList<String> hyperLinks = new ArrayList<String>();
 //
 //        //iterating and extracting
 //        for (Element e:pageElements) {
 //            hyperLinks.add("Text: " + e.text());
-//            hyperLinks.add("Link: " + e.attr("href"));
 //        }
 //
 //        for (String s : hyperLinks) {
 //            System.out.println(s);
+//        }
+//    }
 
-//        java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(java.util.logging.Level.OFF);
-//        java.util.logging.Logger.getLogger("org.apache.http").setLevel(java.util.logging.Level.OFF);
 
         WebClient webClient = new WebClient(BrowserVersion.CHROME);
 
@@ -50,20 +53,33 @@ public class IfpaApplication {
         webClient.getOptions().setJavaScriptEnabled(false);
         webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
         webClient.getOptions().setThrowExceptionOnScriptError(false);
+        webClient.getOptions().setPrintContentOnFailingStatusCode(false);
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 1; i <= 62; i++) {
+            list.add(i);
+        }
 
         //fetching the web page
-        HtmlPage page = webClient.getPage("http://books.toscrape.com/index.html");
+        HtmlPage page = webClient.getPage("https://www.ifpapinball.com/rankings/page-");
 
+        List<?> anchors = page.getByXPath("//li[@class='card-link']");
+        for (int i = 0; i < anchors.size(); i++) {
+            HtmlAnchor link = (HtmlAnchor) anchors.get(i);
+            String recipeTitle = link.getAttribute("title").replace(',', ';');
+            String recipeLink = link.getHrefAttribute();
+        }
+
+
+//   *********** THE BELOW CODE RETURNS DATA ONLY FROM A SINGLE PAGE***********
         //selecting all headings
-        DomNodeList<DomNode> headings = page.querySelectorAll("p, h3");
-
-        //iterating and extracting
-        for (DomNode content: headings) {
+        DomNodeList<DomNode> headings = page.querySelectorAll("title");
+//
+//        //iterating and extracting
+        for (DomNode content : headings) {
             System.out.println(content.asText());
         }
 
     }
 
-
-
-}
