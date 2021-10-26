83f92b0adf1908a93b61e372d9febbe64938fb45
Matty Thompson <matthompson@paypal.com>
2021-10-26 17:07:59 -0500

    WorldRankingsService created

---
+++ src/main/java/com/pinball/ifpa/service/WorldRankingsService.java
@@ -0,0 +1,13 @@
+package com.pinball.ifpa.service;
+
+import com.pinball.ifpa.model.WorldRankings;
+
+import java.util.List;
+
+public interface WorldRankingsService {
+
+    public WorldRankings saveWorldRankings(WorldRankings worldRankings);
+
+    public List<WorldRankings> listWorldRankings();
+
+}
