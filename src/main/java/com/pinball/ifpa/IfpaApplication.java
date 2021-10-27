2acbd70d5704795e7483ab2eb4e7af24e5f7db20
Matty Thompson <matthompson@paypal.com>
2021-10-27 11:29:28 -0500

    isolated player table by XPath

--- src/main/java/com/pinball/ifpa/IfpaApplication.java
+++ src/main/java/com/pinball/ifpa/IfpaApplication.java
@@ -1,21 +1,14 @@
 package com.pinball.ifpa;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.DomNode;
-import com.gargoylesoftware.htmlunit.html.DomNodeList;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.mongodb.client.MongoCollection;
-import com.pinball.ifpa.model.WorldRankings;
+import com.gargoylesoftware.htmlunit.html.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class IfpaApplication {
@@ -26,6 +19,7 @@ public class IfpaApplication {
         try {
             WebClient webClient = new WebClient(BrowserVersion.CHROME);
 
+
             //configuring options
             webClient.getOptions().setUseInsecureSSL(true);
             webClient.getOptions().setCssEnabled(false);
@@ -33,28 +27,15 @@ public class IfpaApplication {
             webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
             webClient.getOptions().setThrowExceptionOnScriptError(false);
 
-
-//        for (int page = 1; page <= 5; page++) {
             //fetching the web page
 
             HtmlPage page = webClient.getPage("https://www.ifpapinball.com/rankings/overall.php?s=w&t=500&p=1");
-//            try {
-            //selecting all headings
-
-            DomNodeList<DomNode> headings = page.querySelectorAll("tbody");
 
 
-//                if (headings.isEmpty()) {
-//                    System.out.println("No items found.");
-//                } else {
-            //iterating and extracting
-            for (DomNode content : headings) {
-                System.out.println(content.asText());
-//
-//                ObjectMapper mapper = new ObjectMapper();
-//                String jsonString = mapper.writeValueAsString(headings);
-//                System.out.println(jsonString);
-            }
+            HtmlTable attributes = (HtmlTable) page.getByXPath("//table[@class='table table-striped table-hover table-sm']").get(0);
+
+                System.out.println(attributes.asText());
+
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/com/pinball/ifpa/repository/TournamentRankingsRepository.java
+++ src/main/java/com/pinball/ifpa/repository/TournamentRankingsRepository.java
@@ -2,6 +2,8 @@ package com.pinball.ifpa.repository;
 
 import com.pinball.ifpa.model.TournamentRanking;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TournamentRankingsRepository extends MongoRepository<TournamentRanking, String> {
 }
